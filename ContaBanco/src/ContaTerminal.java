fe6f84c489dff7960e30b1444549c812b647e4b9
essinformatica <essinformatica15@gmail.com>
2025-05-02 19:55:36 -0300

    desafio finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Criação de variáveis
+
+        Scanner scanner = new Scanner(System.in);
+        // Exibição dos dados do cliente
+        System.out.println("Digite o Nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Digite a Agência: ");
+        int agencia = scanner.nextInt();
+        System.out.println("Digite Número da conta: ");
+        int numero = scanner.nextInt();
+        System.out.println("Digite o Saldo: R$ ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá ".concat(nomeCliente).concat(", obrigado por criar uma conta em nosso banco, sua agência é ") + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
+
 }
